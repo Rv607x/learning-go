45269b73b63f99c0ee686972c851b109c886ac25
Rv607x <jrochola@safaricom.co.ke>
2022-10-17 15:28:21 +0300

    typecasting

---
+++ data_types/typecasting.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number float32 = 5.2 //declare a floating point varible
+	fmt.Println(number)      // print value of variable
+	fmt.Println(int(number)) // print type-cast result
+}
