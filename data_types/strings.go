247d36eae06ca58283a44e10211f868d81b22cc5
Rv607x <jrochola@safaricom.co.ke>
2022-10-17 17:14:09 +0300

    string concatenation

--- data_types/strings.go
+++ data_types/strings.go
@@ -6,4 +6,10 @@ func main() {
 	var cow string = "Hello World!"
 	fmt.Println(cow)
 	fmt.Println(len(cow)) //lenth of string
+
+	// string concatenation
+	s := "hell" + "o "
+	s += "world"
+	fmt.Println(s)
+	fmt.Println(cow[0])
 }
