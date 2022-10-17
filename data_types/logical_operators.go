55758da99a385520d4a46e514dccd6c83f06d987
Rv607x <jrochola@safaricom.co.ke>
2022-10-17 16:33:46 +0300

    boolean logical operators addition

--- data_types/logical_operators.go
+++ data_types/logical_operators.go
@@ -11,4 +11,13 @@ func main() {
 	fmt.Println(b3)
 	b3 = 10 != 10 // not equal to
 	fmt.Println(b3)
+
+	fmt.Println("****************************")
+	// Boolean logical operators below
+	c3 := 10 > 5 && 7 < 15 // AND Operator
+	fmt.Println(c3)
+	c3 = 10 < 5 || 2 > 7 // OR Operator
+	fmt.Println(c3)
+	c3 = !c3 // NOT operator
+	println(c3)
 }
