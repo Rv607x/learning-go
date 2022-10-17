689c2e62ae34996f1e20921289a4cb760d376667
Rv607x <jrochola@safaricom.co.ke>
2022-10-17 15:42:10 +0300

    variables

---
+++ data_types/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int      //declaring and innitializing an integer variable
+	fmt.Println(number) // Printing value of variable
+	var decision bool
+	fmt.Println(decision)
+	var number2 int = 5
+	fmt.Println(number2)
+	var decision2 bool = true
+	fmt.Println(decision2)
+}
